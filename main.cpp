2832fa3053a218c7935914e4abb646a14e4de15b
NaturalBornNerd <laptop>
2023-06-10 18:31:48 -0500

    set up main loop and functions

--- main.cpp
+++ main.cpp
@@ -1,13 +1,88 @@
 #include <iostream>
 #include  "SDL2/SDL.h"
-#include "SDL2/SDL_image.h"
 #include "SDLUtils.h"
 
-int main(int, char**) {
+// Constants
+
+// Window constants
+const int WINDOW_WIDTH = 1000;
+const int WINDOW_HEIGHT = 500;
+const int WINDOW_DEFAULT_XPOS = SDL_WINDOWPOS_UNDEFINED;
+const int WINDOW_DEFAULT_YPOS = SDL_WINDOWPOS_UNDEFINED;
+const int WINDOW_FLAGS = SDL_WINDOW_SHOWN;
+
+// Renderer constants
+const int RENDERER_FLAGS = SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC;
+
+// Initializes the main window and rendering context we will use throughout game
+// window and renderer reference parameters updated with new components
+// returns true on success false on failure
+bool initializeComponents(SDL_Window* &window, SDL_Renderer* &renderer) {
+	// initialize SDL and necessary subsystems
 	if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-		std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
+		SDLUtils::error("SDL_Init");
+		return false;
+	}
+
+	// initialize our window
+	window = SDL_CreateWindow("End of the Earth",
+		WINDOW_DEFAULT_XPOS,
+		WINDOW_DEFAULT_YPOS,
+		WINDOW_HEIGHT,
+		WINDOW_WIDTH,
+		WINDOW_FLAGS
+	);
+
+	if (!window) {
+		SDLUtils::error("SDL_CreateWindow");
+		return false;
+	}
+
+	// Create rendering context
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, RENDERER_FLAGS);
+
+	if (!renderer) {
+		SDLUtils::error("SDL_CreateRenderer");
+		SDL_DestroyWindow(window);
+		return false;
+	}
+
+	return 0;
+}
+
+// Handle relevant user inputs / events
+// Returns bool which indicates whether application should quit
+bool HandleEvents(SDL_Window* window, SDL_Renderer* renderer) {
+	SDL_Event event{};
+
+	while (SDL_PollEvent(&event)) {
+		switch (event.type) {
+			case SDL_QUIT:
+				return true;
+		}
+	}
+}
+
+void RenderFrame(SDL_Window* window, SDL_Renderer* renderer) {
+
+}
+
+int main(int, char**) {
+	SDL_Window* mainWindow = nullptr;
+	SDL_Renderer* renderer = nullptr;
+
+	if (!initializeComponents(mainWindow, renderer)) {
+		SDL_Quit();
 		return 1;
 	}
-	SDL_Quit();
-	return 0;
+	
+	bool readyToQuit = false;
+	SDL_Event event{};
+
+	while (!readyToQuit) {
+		readyToQuit = HandleEvents(mainWindow, renderer); 
+		RenderFrame(mainWindow, renderer);
+		SDL_RenderPresent(renderer); // show new frame
+		SDL_RenderClear(renderer);
+	}
 }
