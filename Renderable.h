b28bd51b4b4bd65f40493597787ef8243556871c
NaturalBornNerd <laptop>
2023-06-11 19:24:01 -0500

    Abstract class for renderable objects

---
+++ Renderable.h
@@ -0,0 +1,13 @@
+// Abstract class that defines required functions for renderable objects
+
+#pragma once
+
+#include <SDL2/SDL.h>
+
+class Renderable {
+	protected:
+		bool shown;
+	public:
+		Renderable() : shown{ false } {};
+		virtual void render(SDL_Renderer* renderer) = 0;
+};
