9d7d4cd2d59a3558ea5e14ea78d5248fd33a7b20
NaturalBornNerd <laptop>
2023-06-11 22:33:54 -0500

    don't need paramter for now

--- IClickable.h
+++ IClickable.h
@@ -6,6 +6,6 @@
 
 class IClickable {
 	public:
-		virtual void mouseDown(SDL_MouseButtonEvent event) = 0;
-		virtual void mouseUp(SDL_MouseButtonEvent event) = 0;
+		virtual void mouseDown() = 0;
+		virtual void mouseUp() = 0;
 };
