85d57de5a0f7fd18d9e5ebbc55fba03aa94d2a02
NaturalBornNerd <laptop>
2023-06-13 00:20:20 -0500

    new/changed constants

--- Button.h
+++ Button.h
@@ -5,6 +5,7 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_ttf.h>
 #include <string>
+#include <cmath>
 #include "SDLUtils.h"
 #include "Renderable.h"
 #include "IClickable.h"
@@ -15,10 +16,10 @@ class Button : public Renderable, public IClickable {
 		inline static const SDL_Color DEFAULT_BORDER_COLOR {0, 0, 0, 255};
 		inline static const int DEFAULT_BORDER_SIZE = 2;
 		inline static const char* FONT_RES_PATH = "fonts";
-		inline static const std::string DEFAULT_FONT = SDLUtils::getResourceDirPath(FONT_RES_PATH) + "pixelFont.ttf";
+		inline static const std::string DEFAULT_FONT = SDLUtils::getResourceDirPath(FONT_RES_PATH) + "sansRegular.ttf";
 		inline static const SDL_Color DEFAULT_FONT_COLOR {0, 0, 0, 255};
-		inline static const int DEFAULT_FONT_SIZE = 24; // text is scaled from here to fit button width
-
+		inline static const int DEFAULT_FONT_SIZE = 60; // text is scaled from here to fit button width
+		inline static const int TEXT_PADDING = 5;
 		int x;
 		int y;
 		int w;
