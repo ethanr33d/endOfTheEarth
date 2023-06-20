0816642b1f9a9e7737a4a817edbaf692f02d95cf
NaturalBornNerd <laptop>
2023-06-20 14:25:14 -0500

    new required getClickBox function for engine to use

--- IClickable.h
+++ IClickable.h
@@ -6,6 +6,11 @@
 
 class IClickable {
 	public:
+
+		// will be called when respective events are detected on object by engine
 		virtual void mouseDown() = 0;
 		virtual void mouseUp() = 0;
+
+		// returns the x,y,w,h of clickable area so the engine can properly look for events
+		virtual SDL_Rect getClickBox() = 0;
 };
