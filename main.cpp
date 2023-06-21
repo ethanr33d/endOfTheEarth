eba06f173221b99f7773eb298ad8888025ee4bf2
NaturalBornNerd <laptop>
2023-06-20 23:17:56 -0500

    using game states

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include "SDLUtils.h"
 #include "Button.h"
 #include "Engine.h"
+#include "MainMenu.h"
 
 // Constants
 const char* GAME_NAME = "End of the Earth";
@@ -10,10 +11,12 @@ const char* GAME_NAME = "End of the Earth";
 int main(int, char**) {
 	Engine gameEngine;
 
-	if (!gameEngine.initializeComponents()) {
+	if (!gameEngine.initializeComponents(GAME_NAME)) {
 		gameEngine.cleanup();
 		return 1;
 	}
+
+	gameEngine.pushGameState(MainMenu(gameEngine));
 
 	bool readyToQuit = false;
 
