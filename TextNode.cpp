fd471df9445395e5ffcecb629c8820bec754e2e8
NaturalBornNerd <ethan.reed9502@gmail.com>
2023-06-26 23:04:25 -0500

    new TextNode class for UI Elements that have text

---
+++ TextNode.cpp
@@ -0,0 +1,71 @@
+#include "TextNode.h"
+
+void TextNode::createTextTexture(SDL_Renderer* renderer) {
+	// open font if it isn't open
+	if (!font) {
+		setFont(DEFAULT_FONT, DEFAULT_FONT_SIZE);
+	}
+
+	textTexture = SDLUtils::createTextTexture(renderer, font, nodeText.c_str(), fontColor);
+
+	SDL_SetTextureScaleMode(textTexture, SDL_ScaleModeBest); // for better text scaling
+}
+
+TextNode::~TextNode() {
+	if (font) TTF_CloseFont(font);
+	if (textTexture) SDL_DestroyTexture(textTexture);
+}
+
+TextNode::TextNode(const std::string& text, const std::string& fontFile, const int fontSize) : 
+		nodeText{ text }, textTexture{ nullptr }, font{ nullptr },  fontSize{fontSize},
+		fontColor{ DEFAULT_FONT_COLOR } { 
+	// open font so text dimensions can be computed, only render texture when it needs to be drawn
+	setFont(fontFile, fontSize);
+	TTF_SizeText(font, nodeText.c_str(), &w, &h);
+}
+
+void TextNode::setFontColor(const SDL_Color& color) {
+	fontColor = color;
+}
+
+void TextNode::setFontSize(const int size) {
+	TTF_SetFontSize(font, size);
+	TTF_SizeText(font, nodeText.c_str(), &w, &h); // recompute dimensions
+}
+
+void TextNode::setText(const std::string& text) {
+	nodeText = text;
+	TTF_SizeText(font, text.c_str(), &w, &h); // recompute dimensions
+
+	// destroy old texture
+	if (textTexture) {
+		SDL_DestroyTexture(textTexture);
+	}
+
+	textTexture = nullptr;
+}
+
+void TextNode::setFont(const std::string& file, int fontSize) {
+	if (font) {
+		TTF_CloseFont(font);
+	}
+
+	font = TTF_OpenFont(file.c_str(), fontSize);
+	TTF_SizeText(font, nodeText.c_str(), &w, &h); // recompute dimensions
+
+	if (!font) {
+		SDLUtils::error("TextNode::setFont OpenFont");
+	}
+}
+
+void TextNode::draw(SDL_Renderer* renderer) {
+	if (!shown) return;
+	SDL_Rect textRect{ x, y, w, h };
+
+	// draw texture if it isn't already drawn
+	if (!textTexture) {
+		createTextTexture(renderer);
+	}
+
+	SDL_RenderCopy(renderer, textTexture, NULL, &textRect);
+}
---
+++ TextNode.h
@@ -0,0 +1,33 @@
+// Wrapper for a text texture with easier access to attributes and text manipulation
+
+#pragma once
+
+#include <string>
+#include "Drawable.h"
+#include "SDLUtils.h"
+
+class TextNode : public Drawable {
+	private:
+		inline static const char* FONT_RES_PATH = "fonts";
+		inline static const std::string DEFAULT_FONT = SDLUtils::getResourceDirPath(FONT_RES_PATH) + "sansRegular.ttf";
+		inline static const SDL_Color DEFAULT_FONT_COLOR {0, 0, 0, 255};
+		inline static const int DEFAULT_FONT_SIZE = 60; // text is scaled from here to fit button width
+
+		std::string nodeText;
+		SDL_Texture* textTexture;
+		TTF_Font* font;
+		int fontSize;
+		SDL_Color fontColor;
+		void createTextTexture(SDL_Renderer* renderer); // helper function for generating texture
+	public:
+		~TextNode();
+		TextNode(const std::string& text, const std::string& fontFile = DEFAULT_FONT,
+			const int size = DEFAULT_FONT_SIZE);
+
+		void setFontColor(const SDL_Color& color);
+		void setFontSize(const int size);
+		void setText(const std::string& text);
+		void setFont(const std::string& font, int fontSize);
+		
+		virtual void draw(SDL_Renderer* renderer);
+};
