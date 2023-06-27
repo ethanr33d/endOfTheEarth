bd603fe63493380651ecfbfcde3bd5ae9c576c8b
NaturalBornNerd <ethan.reed9502@gmail.com>
2023-06-26 22:59:21 -0500

    Dimensions and position now standard part of Drawable objects.

--- Drawable.h
+++ Drawable.h
@@ -7,9 +7,17 @@
 class Drawable {
 	protected:
 		bool shown;
+		int x;
+		int y;
+		int w;
+		int h;
 	public:
-		Drawable() : shown{ false } {};
+		Drawable() : shown{ false }, x{ 0 }, y{ 0 }, w{ 0 }, h{ 0 } {};
 		virtual void draw(SDL_Renderer* renderer) = 0;
 		void show() { shown = true;  };
 		void hide() { shown = false; };
+		void setPosition(const int newX, const int newY) { x = newX; y = newY; };
+		void setSize(const int newW, const int newH) { w = newW, h = newH; };
+
+		SDL_Rect getBounds() { return SDL_Rect{ x,y,w,h }; };
 };
