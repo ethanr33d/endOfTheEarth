d957526219136defa7b1760f7b6290ae28bf8c1c
NaturalBornNerd <ethan.reed9502@gmail.com>
2023-06-26 22:58:20 -0500

    UI Refactor, Button is now special case of TextLabel

--- Button.cpp
+++ Button.cpp
@@ -1,65 +1,20 @@
 #include "Button.h"
 
 // cleanup
-Button::~Button() {
-	TTF_CloseFont(font);
-	SDL_DestroyTexture(textTexture);
-}
 
-void Button::createTextTexture(SDL_Renderer* renderer) {
-	if (!font) {
-		font = TTF_OpenFont(DEFAULT_FONT.c_str(), DEFAULT_FONT_SIZE);
-	}
-
-	if (buttonText == "") {
-		buttonText = " "; // empty string not permitted in text render functions
-	}
-	SDL_Surface* textSurface = TTF_RenderText_Blended(font, buttonText.c_str(), fontColor);
-
-	if (!textSurface) {
-		SDLUtils::error("Button::createTextTexture RenderTextBlend");
-		return;
-	}
-
-	textTexture = SDL_CreateTextureFromSurface(renderer, textSurface);
-
-	if (!textTexture) {
-		SDLUtils::error("Button::createTextTexture TextureFromSurface");
-		return;
-	}
-
-	SDL_SetTextureScaleMode(textTexture, SDL_ScaleModeBest); // for better text scaling
-	SDL_FreeSurface(textSurface);
-}
-
-void Button::setPosition(int xPos, int yPos) {
-	x = xPos;
-	y = yPos;
-}
-
-void Button::setSize(int width, int height) {
-	w = width;
-	h = height;
-}
-
-void Button::setBorder(SDL_Color color, int size) {
-	borderColor = color;
-	borderSize = size;
-}
-
-void Button::setMouseDownHandle(std::function<void()> func) {
+void Button::setMouseDownHandle(const std::function<void()>& func) {
 	downHandler = func;
 }
 
-void Button::setMouseUpHandle(std::function<void()> func) {
+void Button::setMouseUpHandle(const std::function<void()>& func) {
 	upHandler = func;
 }
 
-void Button::setHoverStartHandle(std::function<void()> func) {
+void Button::setHoverStartHandle(const std::function<void()>& func) {
 	hoverStartHandler = func;
 }
 
-void Button::setHoverEndHandle(std::function<void()> func) {
+void Button::setHoverEndHandle(const std::function<void()>& func) {
 	hoverEndHandler = func;
 }
 
@@ -98,40 +53,18 @@ SDL_Rect Button::getHoverBox() {
 
 void Button::draw(SDL_Renderer* renderer) {
 	if (!shown) return;
-	// border is not included in button size
-	SDL_Rect borderRect{ x - borderSize, y - borderSize, w + 2 * borderSize, h + 2 * borderSize};
-	SDL_Rect bgRect{ x, y, w, h };
-	SDL_Rect textRect{ 0,0,0,0 };
 
-	if (!textTexture) {
-		createTextTexture(renderer);
-	}
-
-	SDL_QueryTexture(textTexture, NULL, NULL, &textRect.w, &textRect.h); // get total text dimensions
-	// create border
-	SDL_SetRenderDrawColor(renderer, borderColor.r, borderColor.g, borderColor.b, borderColor.a);
-	SDL_RenderFillRect(renderer, &borderRect);
-
-	// create background
+	// set correct background color
 	if (clicking) {
-		SDL_SetRenderDrawColor(renderer, clickColor.r, clickColor.g, clickColor.b, clickColor.a);
+		bgColor = clickColor;
 	}
 	else if (hover) {
-		SDL_SetRenderDrawColor(renderer, hoverColor.r, hoverColor.g, hoverColor.b, hoverColor.a);
+		bgColor = hoverColor;
 	}
 	else {
-		SDL_SetRenderDrawColor(renderer, bgColor.r, bgColor.g, bgColor.b, bgColor.a);
+		bgColor = idleColor;
 	}
 
-	SDL_RenderFillRect(renderer, &bgRect);
-
-	// create text
-	// scalar for resizing text to fit button, *2 for both sides
-	double scalar = fmin(static_cast<double>(bgRect.w - TEXT_PADDING * 2) / textRect.w, 
-						 static_cast<double>(bgRect.h - TEXT_PADDING * 2) / textRect.h);
-	textRect.w = static_cast<int>(textRect.w * scalar);
-	textRect.h = static_cast<int>(textRect.h * scalar);
-	textRect.x = (x + w / 2) - (textRect.w / 2);
-	textRect.y = (y + h / 2) - (textRect.h / 2);
-	SDL_RenderCopy(renderer, textTexture, NULL, &textRect); // text should only fill background
+	// button is a text label
+	TextLabel::draw(renderer);
 }
--- Button.h
+++ Button.h
@@ -8,64 +8,42 @@
 #include <cmath>
 #include <functional>
 #include "SDLUtils.h"
-#include "Drawable.h"
+#include "TextLabel.h"
 #include "IClickable.h"
 #include "IHoverable.h"
 
-class Button : public Drawable, public IClickable, public IHoverable {
+class Button : public TextLabel, public IClickable, public IHoverable {
 	private:
-		// Constants
+		/* Constants */
+
+		// Override of TextLabel
 		inline static const SDL_Color DEFAULT_BG_COLOR {180, 180, 180, 255};
-		inline static const SDL_Color DEFAULT_BORDER_COLOR {0, 0, 0, 255};
-		inline static const int DEFAULT_BORDER_SIZE = 2;
-		inline static const char* FONT_RES_PATH = "fonts";
-		inline static const std::string DEFAULT_FONT = SDLUtils::getResourceDirPath(FONT_RES_PATH) + "sansRegular.ttf";
-		inline static const SDL_Color DEFAULT_FONT_COLOR {0, 0, 0, 255};
-		inline static const int DEFAULT_FONT_SIZE = 60; // text is scaled from here to fit button width
-		inline static const int TEXT_PADDING = 5;
+
 		inline static const SDL_Color DEFAULT_HOVER_COLOR {128, 128, 128, 255};
 		inline static const SDL_Color DEFAULT_CLICK_COLOR {200, 200, 200, 255};
 
-		int x;
-		int y;
-		int w;
-		int h;
 		bool hover;
 		bool clicking;
 
-		SDL_Color borderColor;
-		int borderSize;
-		SDL_Color bgColor;
-		TTF_Font* font;
-		SDL_Color fontColor;
+		SDL_Color idleColor; // default color
 		SDL_Color hoverColor;
 		SDL_Color clickColor;
 
-		SDL_Texture* textTexture; // so texture does not have to be rendered every frame
 		std::function<void()> downHandler;
 		std::function<void()> upHandler;
 		std::function<void()> hoverStartHandler;
 		std::function<void()> hoverEndHandler;
-		std::string buttonText;
-		void createTextTexture(SDL_Renderer* renderer); // helper function for generating texture
 		
 	public:
-		Button(std::string text = "") : x{ 0 }, y{ 0 }, w{ 0 }, h{ 0 }, hover{false}, 
-			clicking{false}, borderColor{ DEFAULT_BORDER_COLOR }, 
-			borderSize{ DEFAULT_BORDER_SIZE },bgColor{ DEFAULT_BG_COLOR }, 
-			font{ nullptr }, fontColor{DEFAULT_FONT_COLOR}, hoverColor{ DEFAULT_HOVER_COLOR }, 
-			clickColor{ DEFAULT_CLICK_COLOR }, textTexture {nullptr}, downHandler{ nullptr },
-			upHandler{ nullptr }, hoverStartHandler{ nullptr }, hoverEndHandler{ nullptr }, 
-			buttonText {text} {};
+		Button(const std::string& text = "") : TextLabel(text), hover{false}, clicking{false},
+			idleColor{DEFAULT_BG_COLOR}, hoverColor{ DEFAULT_HOVER_COLOR }, 
+			clickColor{ DEFAULT_CLICK_COLOR }, downHandler{ nullptr }, upHandler{ nullptr }, 
+			hoverStartHandler{ nullptr }, hoverEndHandler{ nullptr } {};
 
-		~Button();
-		void setPosition(int xPos, int yPos);
-		void setSize(int width, int height);
-		void setMouseDownHandle(std::function<void()> func);
-		void setMouseUpHandle(std::function<void()> func);
-		void setHoverStartHandle(std::function<void()> func);
-		void setHoverEndHandle(std::function<void()> func);
-		void setBorder(SDL_Color color, int size);
+		void setMouseDownHandle(const std::function<void()>& func);
+		void setMouseUpHandle(const std::function<void()>& func);
+		void setHoverStartHandle(const std::function<void()>& func);
+		void setHoverEndHandle(const std::function<void()>& func);
 
 		// required functions
 		virtual void mouseDown();
