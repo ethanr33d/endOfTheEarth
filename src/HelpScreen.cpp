7e40bf4ca9ae539b282e2bdd2db34e5fef9872b5
NaturalBornNerd <ethan.reed9502@gmail.com>
2023-06-28 00:13:33 -0500

    finish help screen placeholders

--- src/HelpScreen.cpp
+++ src/HelpScreen.cpp
@@ -1,18 +1,42 @@
 #include "HelpScreen.h"
 
 void HelpScreen::backHandle() {
-	
-}
-
-HelpScreen::~HelpScreen() {
-
+	engine.popGameState(); // return to main menu
 }
 
 HelpScreen::HelpScreen(Engine& engine) : GameState(engine), backButton(Button("Back")),
-										helpFont(nullptr), helpTitle(nullptr), helpText(nullptr) {
+		title(TextNode("Help")), helpText(TextNode(HELP_TEXT)) {
+	title.setFontSize(64);
+	
+	helpText.setWrapped(true);
+	helpText.setFontSize(24);
 
+	SDL_Rect titleBounds = title.getBounds();
+	SDL_Rect helpBounds = helpText.getBounds();
+
+	title.setPosition(500 - titleBounds.w / 2, 25); // center horizontally
+	helpText.setPosition(500 - helpBounds.w / 2, 50 + titleBounds.h); // center and place after title
+
+	helpBounds = helpText.getBounds(); // get updated position
+
+	backButton.setSize(200, 75);
+	backButton.setPosition(400, helpBounds.y + helpBounds.h + 25); // position after help text
+
+	backButton.setMouseUpHandle(std::bind(&HelpScreen::backHandle, this));
+
+	registerClickable(&backButton);
+	registerHoverable(&backButton);
+
+	title.show();
+	helpText.show();
+	backButton.show();
 }
 
 void HelpScreen::drawFrame() {
+	SDL_Renderer* renderer = engine.getRenderer();
 
+	MainMenu::drawDefaultBackground(renderer);
+	title.draw(renderer);
+	helpText.draw(renderer);
+	backButton.draw(renderer);
 }
--- src/HelpScreen.h
+++ src/HelpScreen.h
@@ -2,18 +2,23 @@
 #pragma once
 
 #include "GameState.h"
+#include "MainMenu.h"
 #include "UI/Button.h"
 
 class HelpScreen : public GameState {
 	private:
+		inline static const std::string HELP_TEXT = "This is a placeholder \n"
+			"super cool help text goes here \n"
+			"and i do mean super cool \n"
+			"this concludes the tutorial \n"
+			"go test something else now \n";
+
 		Button backButton;
-		TTF_Font* helpFont;
-		SDL_Texture* helpTitle;
-		SDL_Texture* helpText;
+		TextNode title;
+		TextNode helpText;
 
 		void backHandle();
 	public:
-		~HelpScreen();
 		HelpScreen(Engine& engine);
 		virtual void drawFrame();
 };
