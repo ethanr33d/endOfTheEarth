75d8ce7ebfd9d676d210c79f57c15a4c35b885dc
NaturalBornNerd <ethan.reed9502@gmail.com>
2023-07-01 15:15:25 -0500

    bounds now SDL_Rect over individual members, new requires renderer at construction

--- src/UI/Drawable.h
+++ src/UI/Drawable.h
@@ -8,20 +8,20 @@ class Drawable {
 	protected:
 		SDL_Renderer* m_renderer;
 		bool m_shown;
-		int m_x;
-		int m_y;
-		int m_w;
-		int m_h;
+		SDL_Rect m_bounds;
+
 	public:
-		Drawable() : m_shown{ false }, m_x{ 0 }, m_y{ 0 }, m_w{ 0 }, m_h{ 0 } {};
-		virtual void draw(SDL_Renderer* renderer) = 0;
+		Drawable(SDL_Renderer* renderer) : m_renderer{ renderer },
+			m_shown{ false }, m_bounds{ 0,0,0,0 } {};
+
+		virtual void draw() = 0;
 		void show() { m_shown = true;  };
 		void hide() { m_shown = false; };
 
 		// default implementations provided for setPosition and set Size
 		// derived classes can override if extra work needed
-		virtual void setPosition(const int newX, const int newY) { m_x = newX; m_y = newY; };
-		virtual void setSize(const int newW, const int newH) { m_w = newW, m_h = newH; };
+		virtual void setPosition(const int newX, const int newY) { m_bounds.x = newX; m_bounds.y = newY; };
+		virtual void setSize(const int newW, const int newH) { m_bounds.w = newW, m_bounds.h = newH; };
 
-		SDL_Rect getBounds() { return SDL_Rect{ m_x,m_y,m_w,m_h }; };
+		SDL_Rect getBounds() { return m_bounds; };
 };
