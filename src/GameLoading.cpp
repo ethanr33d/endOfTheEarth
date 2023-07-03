069c934f9383648e53394736dcf7f32058f6bb6e
NaturalBornNerd <ethan.reed9502@gmail.com>
2023-07-03 18:21:14 -0500

    register player as keyboard listener

--- src/GameLoading.cpp
+++ src/GameLoading.cpp
@@ -3,6 +3,7 @@
 GameLoading::GameLoading(Engine& engine) : GameState(engine), m_game(GameData(engine.getRenderer())) {
 	m_game.player.setSize(100, 100);
 	m_game.player.setPosition(450, 200);
+	registerKeyboardListener(&m_game.player);
 }
 
 void GameLoading::drawFrame() {
