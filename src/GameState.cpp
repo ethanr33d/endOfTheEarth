f5f76ad6c9598abdb6762b53afc8141675fa4f98
NaturalBornNerd <ethan.reed9502@gmail.com>
2023-07-03 18:21:30 -0500

    game states now have keyboard listener list

--- src/GameState.cpp
+++ src/GameState.cpp
@@ -8,12 +8,20 @@ void GameState::registerHoverable(IHoverable* element) {
 	m_hoverableElements.insert(element);
 }
 
+void GameState::registerKeyboardListener(IKeyboardListener* element) {
+	m_keyboardListeners.insert(element);
+}
+
 std::set<IClickable*>* GameState::getClickableElements() {
 	return &m_clickableElements;
 }
 
 std::set<IHoverable*>* GameState::getHoverableElements() {
 	return &m_hoverableElements;
+}
+
+std::set<IKeyboardListener*>* GameState::getKeyboardListeners() {
+	return &m_keyboardListeners;
 }
 
 std::set<IHoverable*>* GameState::getCurrentlyHoveredElements() {
--- src/GameState.h
+++ src/GameState.h
@@ -13,16 +13,19 @@ class GameState {
 		Engine& m_engine;
 		std::set<IClickable*> m_clickableElements;
 		std::set<IHoverable*> m_hoverableElements;
+		std::set<IKeyboardListener*> m_keyboardListeners;
 		std::set<IHoverable*> m_currentlyHoveredElements;
-
+		
 		// must call these functions to register elements so engine knows to check them for events
 		void registerClickable(IClickable* element);
 		void registerHoverable(IHoverable* element);
+		void registerKeyboardListener(IKeyboardListener* element);
 	public:
 		GameState(Engine& eng) : m_engine(eng) {};
 		virtual void drawFrame() = 0;
 		virtual ~GameState() {}; // so derived classes get properly destroyed
 		std::set<IClickable*>* getClickableElements();
 		std::set<IHoverable*>* getHoverableElements();
+		std::set<IKeyboardListener*>* getKeyboardListeners();
 		std::set<IHoverable*>* getCurrentlyHoveredElements();
 };
