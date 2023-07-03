e4718d5bab67060a46f1ce1723681d3dcfd1f949
NaturalBornNerd <ethan.reed9502@gmail.com>
2023-07-03 18:20:27 -0500

    player responds to keyboard input, goes up/down/right/left

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -1,6 +1,50 @@
 #include "Player.h"
+#include <iostream>
+void Player::adjustMovementMatrix(SDL_Keycode key, bool moveInDirection, int time) {
+	switch (key) {
+		case MOVE_UP_KEY:
+			m_movementMatrix.moveUp = moveInDirection;
+			m_movementMatrix.upKeyTime = time;
+			break;
+		case MOVE_RIGHT_KEY:
+			m_movementMatrix.moveRight = moveInDirection;
+			m_movementMatrix.rightKeyTime = time;
+			break;
+		case MOVE_DOWN_KEY:
+			m_movementMatrix.moveDown = moveInDirection;
+			m_movementMatrix.downKeyTime = time;
+			break;
+		case MOVE_LEFT_KEY:
+			m_movementMatrix.moveLeft = moveInDirection;
+			m_movementMatrix.leftKeyTime = time;
+			break;
+	}
+}
+void Player::keyDown(SDL_Keycode key) {
+	adjustMovementMatrix(key, true, SDL_GetTicks()); // move if key is down
+}
+
+void Player::keyUp(SDL_Keycode key) {
+	adjustMovementMatrix(key, false); // stop moving if key is up
+}
 
 void Player::draw() {
+	int time = SDL_GetTicks();
+
+	// new position = old + (time since last frame or key pressed * move speed * isKeyDown) 
+	// isKeyDown = 1 if true, 0 if false (negates movement)
+	m_exactXPos = m_exactXPos - ((time - m_movementMatrix.leftKeyTime) * (MOVE_SPEED / 1000) * m_movementMatrix.moveLeft);
+	m_exactXPos = m_exactXPos + ((time - m_movementMatrix.rightKeyTime) * (MOVE_SPEED / 1000) * m_movementMatrix.moveRight);
+	m_exactYPos = m_exactYPos - ((time - m_movementMatrix.upKeyTime) * (MOVE_SPEED / 1000) * m_movementMatrix.moveUp);
+	m_exactYPos = m_exactYPos + ((time - m_movementMatrix.downKeyTime) * (MOVE_SPEED / 1000) * m_movementMatrix.moveDown);
+
+	// if key still down, set time to now so next frame only calculates position change since this frame
+	if (m_movementMatrix.moveLeft) m_movementMatrix.leftKeyTime = time;
+	if (m_movementMatrix.moveRight) m_movementMatrix.rightKeyTime = time;
+	if (m_movementMatrix.moveUp) m_movementMatrix.upKeyTime = time;
+	if (m_movementMatrix.moveDown) m_movementMatrix.downKeyTime = time;
+
+	setPosition(static_cast<int>(m_exactXPos), static_cast<int>(m_exactYPos));
 	SDL_SetRenderDrawColor(m_renderer, 255, 0, 0, 255);
 	SDL_RenderFillRect(m_renderer, &m_bounds);
 	SDL_SetRenderDrawColor(m_renderer, 255, 255, 255, 255);
--- src/game/Player.h
+++ src/game/Player.h
@@ -2,9 +2,39 @@
 
 #include "SDL2/SDL.h"
 #include "../Drawable.h"
+#include "../IKeyboardListener.h"
 
-class Player : public Drawable {
+class Player : public Drawable, public IKeyboardListener {
+	private:
+		// constants
+		inline static const SDL_Keycode MOVE_UP_KEY = SDLK_w;
+		inline static const SDL_Keycode MOVE_DOWN_KEY = SDLK_s;
+		inline static const SDL_Keycode MOVE_LEFT_KEY = SDLK_a;
+		inline static const SDL_Keycode MOVE_RIGHT_KEY = SDLK_d;
+		inline static const double MOVE_SPEED = 250; // pixels per second
+
+		struct PlayerMovementMatrix {
+			bool moveLeft; int leftKeyTime; // keyTime = time key was pressed
+			bool moveRight; int rightKeyTime;
+			bool moveUp; int upKeyTime;
+			bool moveDown; int downKeyTime;
+
+			PlayerMovementMatrix() : moveLeft(false), leftKeyTime(0),
+				moveRight(false), rightKeyTime(0),
+				moveUp(false), upKeyTime(0),
+				moveDown(false), downKeyTime(0) {}; // default initializer
+		} m_movementMatrix;
+
+		double m_exactXPos; // exact position for physics calculations
+		double m_exactYPos;
+		int clock;
+
+		// when key event comes in, update matrix as appropriate
+		void adjustMovementMatrix(SDL_Keycode key, bool moveInDirection, int time = 0);
 	public:
-		Player(SDL_Renderer* renderer) : Drawable(renderer) {};
+		Player(SDL_Renderer* renderer) : Drawable(renderer), clock(0) {};
+
+		virtual void keyDown(SDL_Keycode key);
+		virtual void keyUp(SDL_Keycode key);
 		virtual void draw();
 };
