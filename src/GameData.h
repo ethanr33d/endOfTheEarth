681f29ab5d956dcc478e16d919e28beebfa217f5
NaturalBornNerd <ethan.reed9502@gmail.com>
2023-07-07 17:29:39 -0500

    test rects added

--- src/GameData.h
+++ src/GameData.h
@@ -3,10 +3,16 @@
 #pragma once
 
 #include "game/Player.h"
+#include "game/PhysicsEngine.h"
+#include "UI/UIFrame.h"
 
 struct GameData {
 	Player player;
+	UIFrame frame1;
+	UIFrame frame2;
+	UIFrame frame3;
 	// other useful state information here
 
-	GameData(SDL_Renderer* renderer) : player(Player(renderer)) {};
+	GameData(SDL_Renderer* renderer, PhysicsEngine& physicsEngine)
+		: player(Player(renderer, physicsEngine)), frame1(renderer), frame2(renderer), frame3(renderer) {};
 };
