506e82b8284e1ff987ac79ea9804769bb6e411a3
NaturalBornNerd <ethan.reed9502@gmail.com>
2023-07-14 18:27:06 -0500

    new physics element abstract class

---
+++ src/game/PhysicsElement.h
@@ -0,0 +1,48 @@
+// abstract class for all simulated physics elements
+#pragma once 
+
+#include "SDL2/SDL.h"
+#include "Vector2.h"
+#include "PhysicsRect.h"
+
+class PhysicsElement {
+	protected:
+	SDL_Renderer* m_renderer;
+
+	// these should only be set using modifiers below
+	Vector2 m_size;
+	Vector2 m_position;
+	Vector2 m_velocity;
+	Vector2 m_acceleration;
+
+	bool m_anchored;
+	double m_maxVelocity;
+
+	public:
+	PhysicsElement(SDL_Renderer* renderer) : m_renderer(renderer), 
+		m_maxVelocity(INT_MAX), m_anchored(false) {};
+
+	// safe modifiers that add to current vectors
+	void applyVelocity(const Vector2& velocity);
+	void applyAcceleration(const Vector2& acceleration);
+	
+	void setSize(const Vector2& m_size);
+	void setPosition(const Vector2& position);
+	void setVelocity(const Vector2& velocity); 
+	void setAcceleration(const Vector2& acceleration);
+
+	void setAnchored(const bool anchor);
+	void setMaxVelocity(const double maxVelocity);
+
+	Vector2 getSize() const;
+	Vector2 getPosition() const;
+	Vector2 getVelocity() const;
+	Vector2 getAcceleration() const;
+	
+	double getMaxVelocity();
+	bool isAnchored();
+
+	SDL_Rect getIntCastedBounds() const;
+	PhysicsRect getBounds() const;
+	virtual void draw() = 0; // physics elements still need to be drawn
+};
