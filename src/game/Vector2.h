695ae65aaf231ac16cd818de66aa9f23b925d380
NaturalBornNerd <ethan.reed9502@gmail.com>
2023-07-15 01:09:23 -0500

    integer constructors for less casting elsewhere

--- src/game/Vector2.h
+++ src/game/Vector2.h
@@ -4,8 +4,13 @@
 struct Vector2 {
 	double x;
 	double y;
-	Vector2() : x(0.0), y(0.0) {};
+	Vector2() : x(0.0), y(0.0) {}; // default 
+
+	// manual constructors, including int/double combos
 	Vector2(double initX, double initY) : x(initX), y(initY) {};
+	Vector2(int initX, int initY) : x(static_cast<double>(initX)), y(static_cast<double>(initY)) {};
+	Vector2(double initX, int initY) : x(initX), y(static_cast<double>(initY)) {};
+	Vector2(int initX, double initY) : x(static_cast<double>(initX)), y(initY) {};
 
 	/* vector math functions */
 	Vector2 operator+ (const Vector2& vector) {
