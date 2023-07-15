6364089896ee9fa702eff42c7ea2a01b3e89feba
NaturalBornNerd <ethan.reed9502@gmail.com>
2023-07-15 01:06:21 -0500

    temporary testing brick list

--- src/game/GameData.h
+++ src/game/GameData.h
@@ -2,17 +2,16 @@
 // Gets passed between game states
 #pragma once
 
+#include <vector>
 #include "game/Player.h"
 #include "game/PhysicsEngine.h"
 #include "game/Brick.h"
 
 struct GameData {
 	Player player;
-	Brick brick1;
-	Brick brick2;
-	Brick brick3;
+	std::vector<Brick*> brickList;
 	// other useful state information here
 
 	GameData(SDL_Renderer* renderer, PhysicsEngine& physicsEngine)
-		: player(Player(renderer)), brick1(renderer), brick2(renderer), brick3(renderer) {};
+		: player(Player(renderer)) {};
 };
