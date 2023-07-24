5d89e8cdf00262bc797a92d0ba5e48842af12f63
NaturalBornNerd <ethan.reed9502@gmail.com>
2023-07-23 20:29:09 -0500

    update with function name change

--- src/game/Brick.cpp
+++ src/game/Brick.cpp
@@ -5,7 +5,7 @@ void Brick::setColor(const SDL_Color& color) {
 }
 
 void Brick::draw() {
-	SDL_Rect intBounds = getRenderPosition();
+	SDL_Rect intBounds = getRenderBounds();
 
 	SDL_SetRenderDrawColor(m_renderer, m_color.r, m_color.g, m_color.b, m_color.a);
 	SDL_RenderFillRect(m_renderer, &intBounds);
