afe7d1f44157d99226f10f4b989d9e6cfc5a0b1b
NaturalBornNerd <ethan.reed9502@gmail.com>
2023-07-30 22:01:54 -0500

    smoother and more accurate animations. Freefall animation support. Playback for walking varies based on speed and ground friction

--- src/game/Animator.cpp
+++ src/game/Animator.cpp
@@ -3,14 +3,32 @@
 void Animator::decideAnimState() {
 	Vector2 velocity = m_rig->getVelocity();
 
-	if (velocity.y < 0) { // negative Y = up on screen
-		m_animState = JUMPING;
+	if (!m_rig->isGrounded()) { // freefall check
+		if (velocity.y > 0) {
+			m_animState = JUMPING;
+			m_playbackMultiplier = 1;
+		}
+		else {
+			m_animState = FREE_FALL;
+			m_playbackMultiplier = 1;
+		}
 	}
 	else if (abs(velocity.x) > 0) {
 		m_animState = WALKING;
+		// 1 / (% of max velocity player is at) * max speedup * friction of surface to make ice slippy slidy
+		// clamped in the range MAX_FACTOR - MIN_FACTOR
+		m_playbackMultiplier = SDL_clamp(
+			(1 / (abs(velocity.x) / m_rig->getMaxVelocity().x)) * MAX_WALKING_SPEEDFACTOR
+			* m_rig->getGroundingElement()->getFrictionConstant(),
+			MAX_WALKING_SPEEDFACTOR,
+			MIN_WALKING_SPEEDFACTOR
+		);
+		
+		std::cout << m_playbackMultiplier << std::endl;
 	}
 	else {
 		m_animState = IDLE;
+		m_playbackMultiplier = 1;
 	}
 
 	// if velocity.x = 0 keep whatever flip value was previously stored
@@ -23,7 +41,9 @@ void Animator::decideAnimState() {
 
 Animator::Animator(const std::string& sheet, const std::string& sheetData, SDL_Renderer* renderer, 
 	PhysicsElement* element) : m_renderer(renderer), m_spriteSheet(SDLUtils::loadTexture(renderer, sheet)), 
-	m_rig(element), m_animState(IDLE), m_animFrameNumber(-1), m_lastFrame(0), m_flipAnim(false) {
+	m_rig(element), m_animState(IDLE), m_playbackMultiplier(0), m_animFrameNumber(-1), m_lastFrame(0),
+	m_flipAnim(false) {
+
 	std::ifstream dataFile(sheetData);
 
 	if (!dataFile) {
@@ -34,11 +54,16 @@ Animator::Animator(const std::string& sheet, const std::string& sheetData, SDL_R
 	dataFile >> m_sheetData.spriteSizeY;
 	dataFile >> m_sheetData.collisionBoxX;
 	dataFile >> m_sheetData.collisionBoxY;
+	dataFile >> m_sheetData.drawOffsetLeftX;
+	dataFile >> m_sheetData.drawOffsetRightX;
+	dataFile >> m_sheetData.drawOffsetTopY;
+	dataFile >> m_sheetData.drawOffsetBottomY;
 
 	for (int i = 0; i < ANIM_STATE_COUNT; i++) {
 		AnimationSpriteData newAnim{};
 
 		dataFile >> newAnim.frameCount;
+		dataFile >> newAnim.basePlaybackSpeed;
 
 		m_sheetData.animData.push_back(newAnim);
 	}
@@ -53,19 +78,9 @@ void Animator::animate() {
 		m_animFrameNumber = 0; // reset animation if this is a new state
 	}
 
-	if (timeSinceLastFrame > 100) {
-		switch (m_animState) { // increment frame number / loop back to beginning
-			case IDLE:
-				(m_animFrameNumber += 1) %= m_sheetData.animData[IDLE].frameCount;
-				break;
-			case WALKING:
-				(m_animFrameNumber += 1) %= m_sheetData.animData[WALKING].frameCount;
-				break;
-			case JUMPING:
-				(m_animFrameNumber += 1) %= m_sheetData.animData[JUMPING].frameCount;
-				break;
-		}
-
+	if (m_sheetData.animData[m_animState].basePlaybackSpeed * m_playbackMultiplier <= timeSinceLastFrame) {
+		// add to frame number and wrap to 0 if necessary
+		(m_animFrameNumber += 1) %= m_sheetData.animData[m_animState].frameCount; 
 		m_lastFrame = SDL_GetTicks();
 	}
 
@@ -73,10 +88,9 @@ void Animator::animate() {
 
 void Animator::drawAnimationFrame() {
 	SDL_Rect drawablePos = m_rig->getRenderBounds();
-	drawablePos.x -= 31;
-	drawablePos.y -= 21; 
 
-	SDL_Rect spriteClip{
+	// get specific frame of animation
+	SDL_Rect spriteClip{ 
 		m_animFrameNumber * m_sheetData.spriteSizeX,
 		m_animState * m_sheetData.spriteSizeY,
 		m_sheetData.spriteSizeX,
@@ -85,10 +99,21 @@ void Animator::drawAnimationFrame() {
 
 	SDL_RendererFlip flip = SDL_FLIP_NONE;
 
+	double Xscale = static_cast<double>(drawablePos.w) / m_sheetData.spriteSizeX;
+	double Yscale = static_cast<double>(drawablePos.h) / m_sheetData.spriteSizeY;
+
+	int drawOffsetX = static_cast<int>(m_sheetData.drawOffsetLeftX * Xscale);
+	int drawOffsetY = static_cast<int>(m_sheetData.drawOffsetTopY * Yscale);
+
+	// Y flipping not yet supported
 	if (m_flipAnim) {
 		flip = SDL_FLIP_HORIZONTAL;
+		drawOffsetX = static_cast<int>(m_sheetData.drawOffsetRightX * Xscale);
 	}
 
+	// offset drawing so it gets rendered on the ground
+	drawablePos.x -= drawOffsetX;
+	drawablePos.y -= drawOffsetY;
 	SDL_RenderCopyEx(m_renderer, m_spriteSheet, &spriteClip, &drawablePos, 0, NULL, flip);
 }
 
--- src/game/Animator.h
+++ src/game/Animator.h
@@ -9,33 +9,61 @@
 #include <fstream>
 #include <vector>
 
+// Animation sprite sheets should have animations in this order:
+// 1. Idle
+// 2. Walk
+// 3. Jump
+// 4. Freefall
+// 
+// spriteData files take the following form:
+// First 2 numbers: sprite size (ex: 32x32)
+// Numbers 3 and 4: Collision box size of sprite (w,h)
+// Numbers 5 and 6: Pixel offset to the collision box on x-axis, i.e distance in px to drawing from
+//     the left and right side of the sprite
+// Numbers 7 and 8: same as 5 and 6 but for Y
+// Numers 9+: data for each animation in pairs according to animation order (frameCount, playbackSpeed)
+
+
+
 // struct for data on a specific aniamtion
 struct AnimationSpriteData {
 	int frameCount; // how many frames are in the animation
+	int basePlaybackSpeed; // speed in ms between frame changes
 };
+
 struct AnimationSpritesheetData {
 	int spriteSizeX; // size of sprite for rendering
 	int spriteSizeY;
 	int collisionBoxX; // size of the collision box for physics purposes
 	int collisionBoxY;
+	int drawOffsetLeftX;
+	int drawOffsetRightX;
+	int drawOffsetTopY;
+	int drawOffsetBottomY;
 
 	std::vector<AnimationSpriteData> animData;
 
 	AnimationSpritesheetData() : spriteSizeX(0), spriteSizeY(0), collisionBoxX(0),
-		collisionBoxY(0) {};
+		collisionBoxY(0), drawOffsetLeftX(0), drawOffsetRightX(0), drawOffsetTopY(0), 
+		drawOffsetBottomY(0) {};
 };
 
 class Animator {
 	private:
 		// ANIM_TYPE_COUNT for count of animation 
-		enum ANIMATION_STATE {IDLE, WALKING, JUMPING, ANIM_STATE_COUNT};
+		enum ANIMATION_STATE {IDLE, WALKING, JUMPING, FREE_FALL, ANIM_STATE_COUNT};
+
+		// min > max since this multiplies with frame rate (bigger factor = longer time between frames)
+		inline static const double MIN_WALKING_SPEEDFACTOR = 1.5; // minimum playback multiplier for walking 
+		inline static const double MAX_WALKING_SPEEDFACTOR = 0.5; // maximum playback multiplier (character going fast)
 
 		SDL_Renderer* m_renderer;
-		SDL_Texture* m_spriteSheet;
+		SDL_Texture* m_spriteSheet; // current loaded sheet
 		AnimationSpritesheetData m_sheetData;
 		PhysicsElement* m_rig;
 
-		ANIMATION_STATE m_animState;
+		ANIMATION_STATE m_animState; // current animation state
+		double m_playbackMultiplier; // multiplier on top of base playback in certain cases (ex: walking)
 		int m_animFrameNumber; // frame number of current frame in the animation
 		int m_lastFrame; // time since last animation frame
 		bool m_flipAnim; // if animation should be flipped (because player is looking another direction)
